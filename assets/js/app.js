fdd5763855117807100e0a66783bf60bebca6031
OleksandrTishchenko <taa0662621456@gmail.com>
2019-09-17 23:14:36 +0300

    немного доработал модель формы и подключил js сниппет мультистеп-формы. Нужно немного профиксить...

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,17 @@
 // any CSS you require will output into a single css file (app.css in this case)
 require('../scss/app.scss');
 require('../css/app.css');
+
 require('./bootstrap-tags-input-init.js');
 require('./cart.js');
 require('./dropzone');
 require('./js.cookie');
 require('./tinymce_init');
 
-
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 const $ = require('jquery');
+// MultiStepForms init by https://codepen.io/designify-me/pen/qrJWpG
+import('./multistep_forms.js');
+
+
 
