86b19a4263ca729b32346e285600070276d6ea92
Александр <taa0662621456@gmail.com>
2020-04-11 19:13:19 +0300

    рефакторинг главного роута index to homepage

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -11,7 +11,7 @@
 	use Symfony\Component\HttpFoundation\Response;
 	use Symfony\Component\Routing\Annotation\Route;
 
-	class DefaultController
+	class HomepageController
 		extends AbstractController
 	{
 
