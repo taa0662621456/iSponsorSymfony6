0b9b2430864d8552465b977518d65d6a63bc9d85
Александр <taa0662621456@gmail.com>
2020-05-27 21:31:28 +0300

    добавлено включение кеширования на основе навтройки в параметрах

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 
+
 use App\Kernel;
+use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -20,7 +22,20 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false
     Request::setTrustedHosts([$trustedHosts]);
 }
 
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool)$_SERVER['APP_DEBUG']);
+
+/*
+ * добавленно проектом самостоятельно по учебнику Быстрый старт, стр.229
+ * + добавленна собвственная проверка с параметров приложения
+ * Проверить, как работает кеширование на главной странице можно
+ * curl -s -I -X GET https://127.0.0.1:8000/
+ */
+if ('dev' === $kernel->getEnvironment() &&
+    "%app_cache%" != 0) {
+    $kernel = new HttpCache($kernel);
+}
+
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
