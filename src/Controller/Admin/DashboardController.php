c865e3986542b2b86304b0265882f93f39b28d6c
Александр <taa0662621456@gmail.com>
2021-02-11 18:44:40 +0200

    понемногу настраиваю easyAdmin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,6 +17,9 @@ class DashboardController extends AbstractDashboardController
     public function index(): Response
     {
         return parent::index();
+        //$routeBuilder = $this->get(CrudUrlGenerator::class);
+
+        //return $this->redirect($routeBuilder->setController(ProductCrudController::class)->generateUrl());
     }
 
     public function configureDashboard(): Dashboard
@@ -27,14 +31,23 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         //TODO: найти альтернативный способ генерации, не зависящий от названий роутов. Например, занести названия роутов в атрибуты (HPH v8) и перенести/вынести в параметры приложения
+        yield MenuItem::section('Projects');
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linktoRoute('Projects', 'fa fa-home', 'project_index');
-        yield MenuItem::linktoRoute('Projects', 'fa fa-home', 'review_project_index');
+        yield MenuItem::linktoRoute('Reviews', 'fa fa-home', 'review_project_index');
+
+        yield MenuItem::section('Products');
         yield MenuItem::linktoRoute('Products', 'fa fa-home', 'product_index');
-        yield MenuItem::linktoRoute('Products', 'fa fa-home', 'review_product_index');
+        yield MenuItem::linktoRoute('Reviews', 'fa fa-home', 'review_product_index');
+
+        yield MenuItem::section('Users');
         yield MenuItem::linktoRoute('Vendors', 'fa fa-home', 'vendor_index');
         yield MenuItem::linktoRoute('VenCommissions', 'fa fa-home', 'vendor_commissions_index');
+
+        yield MenuItem::section('Attachments');
         yield MenuItem::linktoRoute('Attachments', 'fa fa-home', 'attachment_index');
+
+        yield MenuItem::section('Events');
         yield MenuItem::linktoRoute('Events', 'fa fa-home', 'event_index');
         yield MenuItem::linktoRoute('EvCategories', 'fa fa-home', 'event_categories_index');
         yield MenuItem::linktoRoute('EvMembers', 'fa fa-home', 'event_members_index');
