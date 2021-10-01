af92b99c8a9554fbc79a035a347fcac0e108927c
Alexander Tishchenko <taa0662621456@gmail.com>
2021-10-01 21:20:31 +0300

    req Mercure

--- config/bundles.php
+++ config/bundles.php
@@ -9,5 +9,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
 ];
