72c5c9fee3e4d580d516ee9a9026efd1e088f883
Alexander Tishchenko <taa0662621456@gmail.com>
2021-10-09 20:49:42 +0300

    Объявление событий

---
+++ src/Event/Events.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Event;
+
+final class Events
+{
+    //TODO: проработать на предмет универсальности (продукт, проект, вендор и другие объекты)
+    /**
+     * @Event("Symfony\Component\EventDispatcher\GenericEvent")
+     * @var string
+     */
+    const PRODUCT_CREATED = 'product.created';
+    /**
+     * @Event("Symfony\Component\EventDispatcher\GenericEvent")
+     * @var string
+     */
+    const PRODUCT_UPDATED = 'product.updated';
+    /**
+     * @Event("Symfony\Component\EventDispatcher\GenericEvent")
+     * @var string
+     */
+    const PRODUCT_DELETED = 'product.deleted';
+    /**
+     * @Event("Symfony\Component\EventDispatcher\GenericEvent")
+     * @var string
+     */
+    const ORDER_BEFORE_CREATE = 'order.before_create';
+    /**
+     * @Event("Symfony\Component\EventDispatcher\GenericEvent")
+     * @var string
+     */
+    const ORDER_CREATED = 'order.created';
+    /**
+     * @Event("Symfony\Component\EventDispatcher\GenericEvent")
+     * @var string
+     */
+    const ORDER_DELETED = 'order.deleted';
+    /**
+     * @Event("Symfony\Component\EventDispatcher\GenericEvent")
+     * @var string
+     */
+    const ORDER_STATUS_UPDATED = 'order.status_updated';
+    /**
+     * @Event("Symfony\Component\EventDispatcher\GenericEvent")
+     * @var string
+     */
+    const USER_EMAIL_CONFIRMED = 'user.email_confirmed';
+    /**
+     * @Event("Symfony\Component\EventDispatcher\GenericEvent")
+     * @var string
+     */
+    const SHOPPING_CART_ADD_PRODUCT = 'shopping_cart.add_product';
+}
