c52a394fc49983740e638896ffcc13d6b8966dc5
Alexander Tishchenko <taa0662621456@gmail.com>
2021-10-09 20:43:26 +0300

    Слушатель Доктрины. Подготовка...

---
+++ src/EventSubscriber/DoctrineEventSubscriber.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace App\EventSubscriber;
+
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Events;
+use JetBrains\PhpStorm\ArrayShape;
+
+class DoctrineEventSubscriber implements EventSubscriber
+{
+    //TODO: слушаем доктрину, чтобы в соответствии с CRUD-операциями в БД выполнять CRUD-операции над связанными файлами
+
+
+
+    /**
+     * @return array
+     */
+    #[ArrayShape([Events::preRemove => "string", Events::postRemove => "string", Events::postPersist => "string", Events::postUpdate => "string"])] public function getSubscribedEvents()
+    {
+        return [
+            Events::preRemove => 'preRemove',
+            Events::prePersist => 'prePersist',
+            Events::postRemove => 'postRemove',
+            Events::postPersist => 'postPersist',
+            Events::postUpdate => 'postUpdate'
+        ];
+    }
+
+    public function postUpdate()
+    {
+
+    }
+    public function postPersist()
+    {
+
+    }
+    public function postRemove()
+    {
+
+    }
+    public function preRemove()
+    {
+
+    }
+
+
+}
