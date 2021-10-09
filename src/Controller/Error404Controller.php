4619151ad94e7a315d0f2e685ddef447b4676c04
Alexander Tishchenko <taa0662621456@gmail.com>
2021-10-09 20:38:43 +0300

    added Error404Controller.php

---
+++ src/Controller/Error404Controller.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Error404Controller extends AbstractController
+{
+    /**
+     * @Route("/404", name="404")
+     * @return Response
+
+     */
+    public function pageNotFoundAction(): Response
+    {
+        return $this->render('_404.html.twig', [
+            'currentUri' => '404'
+        ]);
+    }
+
+}
