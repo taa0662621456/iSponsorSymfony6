916e720dbee7b5f357a520b7c61de9627fac7cc3
Alexander Tishchenko <taa0662621456@gmail.com>
2021-10-27 11:51:55 +0300

    VendorEntity commit

---
+++ src/Entity/Vendor/VendorFavourite.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Entity\Vendor;
+
+use App\Entity\BaseTrait;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="vendors_favourites", indexes={
+ * @ORM\Index(name="vendor_favourite_idx", columns={"slug"})})
+ * @ORM\Entity(repositoryClass="App\Repository\Vendor\VendorRepository")
+ */
+class VendorFavourite
+{
+	use BaseTrait;
+
+	/**
+	 * @ORM\ManyToMany(targetEntity="App\Entity\Vendor\Vendor", inversedBy="vendorFavourites")
+	 * @ORM\JoinColumn(name="vendorFavourites_id", referencedColumnName="id")
+	 */
+	private $vendorFavourites;
+
+
+	/**
+	 * @return int
+	 */
+	public function getVendorFavourites(): int
+	{
+		return $this->vendorFavourites;
+	}
+
+	/**
+	 * @param int $vendorFavourites
+	 */
+	public function setVendorFavourites(int $vendorFavourites): void
+	{
+		$this->vendorFavourites = $vendorFavourites;
+	}
+}
