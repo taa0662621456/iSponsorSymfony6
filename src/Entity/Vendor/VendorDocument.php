8fca8e31ef92fd11f32be9ae531a33b893e08327
Alexander Tishchenko <taa0662621456@gmail.com>
2021-11-08 14:14:54 +0200

    fix

--- src/Entity/Vendor/VendorDocument.php
+++ src/Entity/Vendor/VendorDocument.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace App\Entity\Vendor;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Entity\AttachmentTrait;
 use App\Entity\BaseTrait;
 
@@ -13,6 +16,14 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Index(name="vendor_attachment_idx", columns={"slug"})}))
  * @ORM\Entity(repositoryClass="App\Repository\Vendor\VendorRepository")
  * @ORM\HasLifecycleCallbacks()
+ *
+ * @ApiResource(
+ *     collectionOperations={"get"={"normalization_context"={"groups"="document:list"}}},
+ *     itemOperations={"get"={"normalization_context"={"groups"="document:item"}}},
+ *     order={"createdAt"="DESC"},
+ *     paginationEnabled=false
+ *     )
+ * @ApiFilter(SearchFilter::class, properties={"vendor": "exact"})
  */
 class VendorDocument
 {
