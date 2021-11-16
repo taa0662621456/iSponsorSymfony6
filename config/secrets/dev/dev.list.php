1eccccc2e059eaea16e2fbfe28de5392d4e412cd
Alexander Tishchenko <taa0662621456@gmail.com>
2021-11-16 12:57:03 +0200

    .env secrets commit

---
+++ config/secrets/dev/dev.list.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'APP_SECRET' => null,
+    'DATABASE_URL' => null,
+    'FACEBOOK_OAUTH_ID' => null,
+    'FACEBOOK_OAUTH_SECRET' => null,
+    'GOOGLE_AUTH_ID' => null,
+    'GOOGLE_AUTH_SECRET' => null,
+    'GOOGLE_RECAPTCHA_KEY' => null,
+    'GOOGLE_RECAPTCHA_SECRET' => null,
+    'OBJECT_REVIEW_DATABASE' => null,
+    'PLATFORM_DATE_DATABASE' => null,
+    'SMS_GATWAY_ID' => null,
+    'SMS_GATWAY_PASSWORD' => null,
+    'SMS_STATUS_SECRET' => null,
+];
