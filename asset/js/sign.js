1246e372a1549d73e5e05548c88dd9d7f1890e80
Alexander Tishchenko <taa0662621456@gmail.com>
2021-11-16 18:41:28 +0200

    body style fix and signin EntryPoint fix

--- asset/js/sign.js
+++ asset/js/sign.js
@@ -1,4 +1,4 @@
-require('../scss/app.scss');
-require('../css/app.css');
+require('../scss/sign.scss');
+require('../css/sign.css');
 require('../../templates/bootstrap-4.0.0/docs/4.0/examples/sign-in/signin.css');
 
