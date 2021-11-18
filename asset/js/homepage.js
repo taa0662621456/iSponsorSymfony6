bfa583347358be0738e53a383f695fe0600aec94
Alexander Tishchenko <taa0662621456@gmail.com>
2021-11-18 19:11:26 +0200

    added EntryPoint homepage.js

---
+++ asset/js/homepage.js
@@ -0,0 +1,32 @@
+require('jquery');
+//import 'jquery';
+//require('jquery-bridget'); // для использования Masonry
+require('bootstrap');
+// require('jquery-confirm');
+// require('bootstrap-autohide-navbar');
+
+// require('smartwizard');
+
+//require('masonry-layout');
+require('../scss/homepage.scss');
+require('../css/homepage.css');
+
+
+//import('./masonry_init.js');
+//import('../css/app.css');
+// import('../css/navbar.css');
+
+
+// //require('../css/likeMasonryCart.css');
+// require('../fontawesome-pro/js/all.min');
+// require('./auto_hiding_navbar_init');
+// require('./bootstrap-tags-input-init');
+// require('./cart.js');
+// //require('./multistep_form');
+// require('./tinymce_init');
+// require('./smartwizard_init');
+// require('./move_up');
+// //require('./masonry_init');
+// require('./cols_per_row');
+// require('./add-collection-widget');
+// require('./full_screen');
