c5a2a2c3e6ff2c9ce11657a16b994c904524062b
Alexander Tishchenko <taa0662621456@gmail.com>
2021-12-05 22:09:43 +0200

    refactoring/renamed

---
+++ src/Service/EmailConfirmation.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
+
+class EmailConfirmation
+{
+    private VerifyEmailHelperInterface $verifyEmailHelper;
+    private MailerInterface $mailer;
+
+    public function __construct(VerifyEmailHelperInterface $helper, MailerInterface $mailer)
+    {
+        $this->verifyEmailHelper = $helper;
+        $this->mailer = $mailer;
+    }
+
+    /**
+     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
+     */
+    public function confirmationSignatureSender(string $confirmationRouteName, UserInterface $vendorSecurity, TemplatedEmail $email): void
+    {
+        $signature = $this->verifyEmailHelper->generateSignature(
+            $confirmationRouteName,
+            $vendorSecurity->getId(),
+            $vendorSecurity->getEmail(),
+            ['slug' => $vendorSecurity->getSlug()]
+        );
+
+        $context = $email->getContext();
+        $context['signedUrl'] = $signature->getSignedUrl();
+        $context['expiresAtMessageKey'] = $signature->getExpirationMessageKey();
+        $context['expiresAtMessageData'] = $signature->getExpirationMessageData();
+
+        $email->context($context);
+
+        $this->mailer->send($email);
+    }
+}
--- src/Service/SmsConfirmation.php
+++ src/Service/SmsConfirmation.php
@@ -4,7 +4,7 @@
 namespace App\Service;
 
 
-class SmsVerifier
+class SmsConfirmation
 {
 
 }
