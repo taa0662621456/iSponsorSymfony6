fa2b9e755afec636c61c7040fdcddfced025226d
Oleksandr Tishchenko <taa0662621456@gmail.com>
2022-12-22 17:22:28 -0800

    Exception

---
+++ src/Exception/NonExistingServiceException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Exception;
+
+/**
+ * This exception should be thrown by service registry
+ * when given service type does not exist.
+ */
+class NonExistingServiceException extends \InvalidArgumentException
+{
+    public function __construct(string $context, string $type, array $existingServices)
+    {
+        parent::__construct(sprintf(
+            '%s service "%s" does not exist, available %s services: "%s"',
+            ucfirst($context),
+            $type,
+            $context,
+            implode('", "', $existingServices)
+        ));
+    }
+}
