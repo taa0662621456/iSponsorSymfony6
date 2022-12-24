f85c5adcc7ca1090f83a08a18b45736643f9f61f
Oleksandr Tishchenko <taa0662621456@gmail.com>
2022-12-23 22:15:58 -0800

    TranslationCommand

--- src/Command/LanguageTableCommand.php
+++ src/Command/LanguageTableCommand.php
@@ -1,57 +1,54 @@
 <?php
 
+
 namespace App\Command;
 
-use App\Service\ObjectInitializer;
+
+use App\Service\RequestDispatcher;
 use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Exception;
-use Doctrine\Orm\NoResultException;
-use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Command\LockableTrait;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Command\LockableTrait;
 use Symfony\Component\Console\Output\OutputInterface;
-use function count;
 
-/**
- * @property $requestDispatcher
- */
 class LanguageTableCommand extends Command
 {
     use LockableTrait;
 
     private ManagerRegistry $managerRegistry;
-
     private Connection $connection;
 
     protected static $defaultDescription = 'Generate Object language Table in Postgres. Эта команда создает таблицу объекта в базе данных с использованием аргумента - суффикса/постфикса - языка. Например: ObjectRuRU или ObjectMediaRuRU. RuRU - суффикс';
-
     protected static $defaultName = 'generate-translation-table';
 
-    public function __construct(
-        ManagerRegistry $managerRegistry,
-        private readonly ObjectInitializer $objectInitializer,
-        Connection $connection,
-        string $name = null
-    ) {
+    public function __construct(ManagerRegistry                    $managerRegistry,
+                                private readonly RequestDispatcher $requestDispatcher,
+                                Connection $connection,
+                                string $name = null)
+    {
         parent::__construct($name);
         $this->managerRegistry = $managerRegistry;
         $this->connection = $connection;
     }
 
-    protected function configure(): void
+    protected function configure()
     {
         $this
             ->setDescription('Generate Object language Table in Postgres')
-            ->addArgument('object', InputArgument::OPTIONAL | InputArgument::IS_ARRAY, 'Object Entity Name(s). Ex.: Object ObjectAttachment ... .  Use space as separator')
-            ->addArgument('language', InputArgument::OPTIONAL | InputArgument::IS_ARRAY, 'Language Entity Suffix(es). Ex. "Object [RuRU] .php". Use space as separator')
+            ->addArgument('object', InputArgument::OPTIONAL|InputArgument::IS_ARRAY, 'Object Entity Name(s). Ex.: Object ObjectAttachment ... .  Use space as separator')
+            ->addArgument('language', InputArgument::OPTIONAL|InputArgument::IS_ARRAY, 'Language Entity Suffix(es). Ex. "Object [RuRU] .php". Use space as separator')
             ->setHelp('Эта команда создает таблицу объекта в базе данных с использованием аргумента - суффикса/постфикса - языка. Например: ObjectRuRU или ObjectMediaRuRU. RuRU - суффикс')
-            ->setHidden();
+            ->setHidden(true)
+        ;
     }
 
     /**
-     * @throws Exception
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     * @throws \Doctrine\DBAL\Exception
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
@@ -62,18 +59,20 @@ class LanguageTableCommand extends Command
         }
 
         $output->writeln('Create Translation table');
-        $output->writeln('Object. Press Enter for empty: '.$input->getArgument('object'));
-        $output->writeln('Language. Press Enter for empty: '.$input->getArgument('language'));
+        $output->writeln('Object. Press Enter for empty: ' .$input->getArgument('object'));
+        $output->writeln('Language. Press Enter for empty: ' .$input->getArgument('language'));
 
         $object = $input->getArgument('object');
         $language = $input->getArgument('language');
 
-        if (0 != count($object) || 0 != count($language)) {
+        if (0 != count($object) OR 0 != count($language)){
+
             $summery = [];
 
-            for ($o = 0; $o <= count($object); $o++) {
+            for ($o = 0; $o <= count($object); $o++ ) {
                 for ($l = 0; $l <= count($language); $l++) {
-                    $r = $object[$o].$language[$l];
+
+                    $r = $object[$o] . $language[$l];
 
                     $summery[] = [$r];
 
@@ -81,18 +80,21 @@ class LanguageTableCommand extends Command
                 }
             }
 
-            for ($i = 0; $i <= count($summery); $i++) {
-                $o = $object[$i].'EnUS';
+            for ($i = 0; $i <= count($summery); $i++){
+
+                $o = $object[$i] . 'EnUS';
 
                 $this->tableQuery($summery[$i], $o);
+
             }
         }
 
-        //        if (null == $object AND null == $language) {
-        if (!$object && !$language) {
+//        if (null == $object AND null == $language) {
+        if (!$object AND !$language) {
+
             $object = $this->requestDispatcher->object();
-            $objectEnUS = $object.'EnUS';
-            $object = $object.$this->requestDispatcher->localeFilter();
+            $objectEnUS = $object . 'EnUS';
+            $object = $object . $this->requestDispatcher->localeFilter();
 
             $this->tableQuery($object, $objectEnUS);
         }
@@ -101,15 +103,15 @@ class LanguageTableCommand extends Command
     }
 
     /**
-     * @throws Exception
+     * @throws \Doctrine\DBAL\Exception
      */
     public function tableQuery(string $object, string $objectEnUS): void
     {
         $em = $this->managerRegistry->getManager();
 
         $q = $em->createQuery(
-            'CREATE TABLE IF NOT EXISTS'.$object.'
-             AS (SELECT 1 FROM information_schema.columns WHERE table_name ='.$objectEnUS.')
+            'CREATE TABLE IF NOT EXISTS' . $object . '
+             AS (SELECT 1 FROM information_schema.columns WHERE table_name =' . $objectEnUS . ')
              WITH NO DATA'
         );
 
@@ -118,8 +120,9 @@ class LanguageTableCommand extends Command
 
         try {
             $t = $q->getSingleResult();
-        } catch (NoResultException $e) {
+        } catch (\Doctrine\Orm\NoResultException $e) {
             $t = null;
         }
+
     }
 }
--- src/Command/Setup/SetupLocaleCommand.php
+++ src/Command/Setup/SetupLocaleCommand.php
@@ -1,12 +1,95 @@
 <?php
+
 namespace App\Command\Setup;
+
+use App\Entity\Locale\Locale;
+use App\Interface\Currency\CurrencyInterface;
+use App\Repository\LocaleRepository;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-class SetupLocaleCommand extends AbstractSetupCommand {
-    protected static $defaultName = 'app:setup:locale';
-    protected function execute(InputInterface $in, OutputInterface $out): int {
-        $out->writeln('Locales setup completed.');
-        return Command::SUCCESS;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Intl\Exception\MissingResourceException;
+use Symfony\Component\Intl\Locale as Intl;
+
+
+class SetupLocaleCommand extends Command
+{
+    protected static $defaultName = 'setup.locale';
+    private string $locale;
+
+    public function __construct(
+        private readonly LocaleRepository $localeRepository,
+        string                            $locale = 'en-US',
+    ) {
+        parent::__construct();
+        $this->locale = $locale;
     }
+
+    public function setup(InputInterface $input, OutputInterface $output, QuestionHelper $questionHelper, ?ObjectManager $manager): string
+    {
+        $locale = $this->localeCodeQuestion($input, $output, $questionHelper);
+        $localeRepository = $this->getApplication()->get('repository.locale');
+
+        /** @var CurrencyInterface|null $existingCurrency */
+        $existingCurrency = $this->localeRepository->findOneBy(['locale' => $locale]);
+
+//        if (null !== $existingCurrency) {
+//            return $existingCurrency;
+//        }
+
+
+        $localeEntity = new Locale();
+
+        $localeEntity->setSlug($locale);
+
+        $manager->persist($localeEntity);
+
+        /*
+        /** @var CurrencyInterface $currency *
+        $currency = $this->currencyFactory->createNew();
+        $currency->setCode($code);
+        $this->currencyRepository->add($currency);
+         */
+
+        return $locale;
+    }
+
+    private function localeCodeQuestion(InputInterface $input, OutputInterface $output, QuestionHelper $questionHelper): string
+    {
+        $code = $this->localeCode($input, $output, $questionHelper);
+        $name = $this->getLocaleName($code);
+
+        while (null === $name) {
+            $output->writeln(
+                sprintf('<comment>Currency with code <info>%s</info> could not be resolved.</comment>', $code),
+            );
+
+            $code = $this->localeCode($input, $output, $questionHelper);
+            $name = $this->getLocaleName($code);
+        }
+
+        $output->writeln(sprintf('Adding <info>%s</info> currency.', $name));
+
+        return $code;
+    }
+
+    private function localeCode(InputInterface $input, OutputInterface $output, QuestionHelper $questionHelper): string
+    {
+        $question = new Question(sprintf('Currency (press enter to use %s): ', $this->locale), $this->locale);
+
+        return trim($questionHelper->ask($input, $output, $question));
+    }
+
+    private function getLocaleName(string $code): ?string
+    {
+        try {
+            return Intl::getDisplayName($code);
+        } catch (MissingResourceException) {
+            return null;
+        }
+    }
+
 }
