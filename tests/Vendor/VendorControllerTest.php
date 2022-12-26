7d426e49c76d2378eb36a3b180b8d629974fa5b7
Oleksandr Tishchenko <taa0662621456@gmail.com>
2022-12-25 22:35:03 -0800

    add some new classes and func...

---
+++ tests/Vendor/VendorControllerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Vendor;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class VendorControllerTest extends WebTestCase
+{
+    public function vendorGetAllTest()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/vendor');
+
+        $this->assertResponseIsSuccessful();
+
+        $response = $client->getResponse();
+        $data = $response->getContent();
+
+        $this->assertStringContainsString('Vendor', $data);
+    }
+
+}
---
+++ tests/Vendor/VendorEnRepositoryTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Vendor;
+
+
+use App\Factory\Vendor\VendorEnUsFactory;
+use App\Repository\Vendor\VendorEnGbRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class VendorEnRepositoryTest extends KernelTestCase
+{
+    private EntityManagerInterface $entityManager;
+    private VendorEnGbRepository $vendorEnGbRepository;
+
+    public function vendorEnCreateTest()
+    {
+        $vendorEn = VendorEnUsFactory::createVendorEnUsEntity('Mike', 'Douglas');
+
+        $this->entityManager->persist($vendorEn);
+        $this->entityManager->flush();
+
+        $this->assertNotNull($vendorEn->getId());
+
+        $findVendorEnById = $this->vendorEnGbRepository->findOneBy(['id' => $vendorEn->getId()]);
+
+        $this->assertEquals('test VendorEn firstTitle', $findVendorEnById->getFirstTitle());
+        $this->assertEquals('test VendorEn lastTitle', $findVendorEnById->getLastTitle());
+    }
+
+
+
+    public function setup(): void
+    {
+
+        // first step: boot the Symfony kernel
+        $kernel = self::bootKernel();
+
+        $this->assertSame('test', $kernel->getEnvironment());
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager()
+        ;
+
+        // second step: get VendorEnRepository from container
+        $container = static :: getContainer();
+        $this->vendorEnGbRepository = $container->get(VendorEnGbRepository::class);
+
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        $this->entityManager->close();
+    }
+
+
+
+}
