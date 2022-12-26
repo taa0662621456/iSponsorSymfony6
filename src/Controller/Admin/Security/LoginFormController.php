85f13b5083f32d2e632fe1b78951d8ff75f4bc9d
Oleksandr Tishchenko <taa0662621456@gmail.com>
2022-12-25 22:33:30 -0800

    Controller refactoring

--- src/Controller/Admin/Security/LoginFormController.php
+++ src/Controller/Admin/Security/LoginFormController.php
@@ -2,6 +2,7 @@
 namespace App\Controller\Admin\Security;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
