15278b846f5b35364351101b5b00d7bfe8dbb536
Oleksandr Tishchenko <taa0662621456@gmail.com>
2022-12-25 22:32:50 -0800

    Command

---
+++ src/Command/Vendor/VendorEnGbCreateCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Command\Vendor;
+
+use _PHPStan_582a9cb8b\Symfony\Component\Console\Attribute\AsCommand;
+use App\Factory\Vendor\VendorEnUsFactory;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(name: 'add-vendor', description: 'Add a titles for your command')]
+class VendorEnGbCreateCommand extends Command
+{
+    public function __construct(private readonly EntityManagerInterface $manager)
+    {
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+
+        $this
+            ->addArgument('firstTitle', InputArgument::REQUIRED, 'Vendor first title/first name')
+            ->addArgument('lastTitle',InputArgument::REQUIRED, 'Vendor last title/last name')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $firstTitle = $input->getArgument('title');
+
+        if ($firstTitle) {
+            $io->note(sprintf('First title/first name: %s', $firstTitle));
+        }
+
+        $lastTitle = $input->getArgument('lastTitle');
+
+        if ($lastTitle) {
+            $io->note(sprintf('Content: %s', $lastTitle));
+        }
+
+        $vendorEnUs = VendorEnUsFactory::createVendorEnUsEntity($firstTitle, $lastTitle);
+        $this->manager->persist($vendorEnUs);
+        $this->manager->flush();
+
+        $io->success('Vendor is saved: ' . $vendorEnUs);
+
+        return Command::SUCCESS;
+    }
+}
