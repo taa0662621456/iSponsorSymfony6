e51c098a60fe405c5bb66a55f50e0f18e728ee4c
Oleksandr Tishchenko <oleksandr@example.com>
2025-09-24 20:00:00 +0000

    Refactor commands: structure, base classes, services.yaml, README, monitoring

--- src/Controller/Admin/Security/LoginFormController.php
+++ src/Controller/Admin/Security/LoginFormController.php
@@ -2,13 +2,11 @@
 namespace App\Controller\Admin\Security;
 
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-#[AsController]
 class LoginFormController extends AbstractController
 {
     #[Route('/admin/login', name: 'admin_login')]
