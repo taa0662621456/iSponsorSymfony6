8210ce0c32c6aa430ba538cf5ece13d17f352430
Oleksandr Tishchenko <taa0662621456@gmail.com>
2025-09-26 23:34:06 -0500

    Update prompt controller response

--- src/Controller/GptConsulterController.php
+++ src/Controller/GptConsulterController.php
@@ -4,20 +4,20 @@ namespace App\Controller;
 
 
 use App\Service\GptExceptionConsulter;
-use JetBrains\PhpStorm\NoReturn;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[AsController]
 class GptConsulterController extends AbstractController
 {
-    #[NoReturn] #[Route(path: 'prompt', name: 'prompt', methods: ['GET', 'POST'])]
-    public function prompt(GptExceptionConsulter $gptExceptionConsulter): string
+    #[Route(path: 'prompt', name: 'prompt', methods: ['GET', 'POST'])]
+    public function prompt(GptExceptionConsulter $gptExceptionConsulter): JsonResponse
     {
-        $result = $gptExceptionConsulter->generateComment('sdsd', 'sdsd');
-
-        return $this->json(['generated_text' => $result]);
+        return $this->json([
+            'generated_text' => $gptExceptionConsulter->generateComment('sdsd', 'sdsd'),
+        ]);
     }
 
 }
